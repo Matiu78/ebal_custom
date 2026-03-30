8885e1b22c40f857ea02f05071e57d6d0eb1002e
AyniMatiu <mreinoso@neogisp.com>
2026-03-30 22:34:12 +0200

    [ADD] ebal_diegovelez Creation of custom module

--- ebal_diegovelez/__manifest__.py
+++ ebal_diegovelez/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Diego Velez",
     "version": "18.0.1.0.0",
-    "summary": "Diego Velez,
+    "summary": "Diego Velez",
     "author": "EbalSoft",
     "category": "Diego Velez",
     "depends": [
--- ebal_diegovelez/report/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import income_by_mechanic_xlsx
-from . import income_labor_and_parts_xlsx
